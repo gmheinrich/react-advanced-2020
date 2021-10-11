1df671f01dabded1e421ca267ee4570faaff4c14
gmheinrich <gmheinrich@gmail.com>
2021-10-11 14:16:49 -0300

    1-useState

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  
+  let title = 'Random Title';
+  
+  const handleClick = () => {
+    title = 'hello people';
+    console.log(title);
+  };
+  
+  return (
+    <React.Fragment>
+      <h2>{title}</h2>
+      <button type="button" className="btn" onClick={handleClick}>
+        Change title
+      </button>
+    </React.Fragment>  
+  );
 };
 
 export default ErrorExample;
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+
+  const [person, setPerson] = useState({ name: 'John', age: 24, message: 'random message'});
+
+  const changeMessage = () => {
+    setPerson( { ...person, message: 'Hello world' } )
+  };
+
+  return (
+    <>
+      <h4>{person.name}</h4>
+      <h4>{person.age}</h4>
+      <h4>{person.message}</h4>
+      <button className="btn" onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
